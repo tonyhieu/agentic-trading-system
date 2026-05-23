4767ed002c8a5de2368b31bc4de2ed216ceedb71
frank22lin <franklin.liu22@googlemail.com>
2026-05-23 14:31:57 -0500

    afg-f-l8: completed [full-trace loop 8], pnl=1672.75, sharpe=7.37

--- execution_algos/__init__.py
+++ execution_algos/__init__.py
@@ -85,6 +85,10 @@ _EXEC_ALGORITHM_FACTORIES: dict[str, tuple[str, str]] = {
         "execution_algos.afg-f-l7",
         "get_execution_algorithm",
     ),
+    "afg-f-l8": (
+        "execution_algos.afg-f-l8",
+        "get_execution_algorithm",
+    ),
     "vrs-m-l1": (
         "execution_algos.vrs-m-l1",
         "get_execution_algorithm",
