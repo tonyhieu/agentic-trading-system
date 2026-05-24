37aee9a470176b8d5c5bef9d3dd6690cd918b9a9
frank22lin <franklin.liu22@gmail.com>
2026-05-23 22:13:47 -0500

    vrs-isl-g1l1: island g1l1, pnl=1011.0, sharpe=5.97

--- execution_algos/__init__.py
+++ execution_algos/__init__.py
@@ -101,6 +101,10 @@ _EXEC_ALGORITHM_FACTORIES: dict[str, tuple[str, str]] = {
         "execution_algos.afg-isl-g1l1",
         "get_execution_algorithm",
     ),
+    "vrs-isl-g1l1": (
+        "execution_algos.vrs-isl-g1l1",
+        "get_execution_algorithm",
+    ),
 }
 
 
