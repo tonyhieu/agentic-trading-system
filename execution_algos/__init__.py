8bade2ff6ef48ec9ad4ec55e727063d4a9263c34
island-experiment <researcher@island-experiment.local>
2026-05-24 13:03:23 +0000

    afg-isl-g2l2: island g2l2, pnl=3439.5, sharpe=14.45

--- execution_algos/__init__.py
+++ execution_algos/__init__.py
@@ -121,6 +121,10 @@ _EXEC_ALGORITHM_FACTORIES: dict[str, tuple[str, str]] = {
         "execution_algos.afg-isl-g2l1",
         "get_execution_algorithm",
     ),
+    "afg-isl-g2l2": (
+        "execution_algos.afg-isl-g2l2",
+        "get_execution_algorithm",
+    ),
     "vrs-isl-g1l1": (
         "execution_algos.vrs-isl-g1l1",
         "get_execution_algorithm",
